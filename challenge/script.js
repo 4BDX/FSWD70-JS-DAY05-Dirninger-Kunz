b08cd9b96a0821bf4a57f2a279780a5e0d94df66
Tanja Kunz <tk.kunz@gmx.at>
2019-05-23 15:14:33 +0200

    calculation

--- challenge/script.js
+++ challenge/script.js
@@ -1,21 +1,56 @@
+var cart = [10, 20, 30]/*new Array()*/;
+var shipping = new Array();
+
+/*cart.push(); // Preis für hinzugefügtes Produkt in Array Cart
+shipping.push(shoes[].shipping);*/
+var cartTotal = 0;
+var shipTotal = 0;
+
+function sumUp (arr) {
+	var result = 0;
+	for (i = 0; i < arr.length; i++) {
+	result = result + arr[i];
+	}
+	return result;
+}
 
 
+document.write(cartTotal);
 
+
+function addToCart(obj){
+	shipping.push(obj);
+	console.log(shipping);
+}
+
+var btn = document.getElementsByTagName("button");
+
+for(let i = 0 ; i< btn.length; i++){
+	btn[i].addEventListener("click",function(){addToCart(shoes[i])},false);
+}
 
 // ----- discount calculation -----
 
-var totalCost = prompt("Preis");
+var totalCost;
 var discount;
 
 if (totalCost < 40) {
 	discount = 0;
 } else if (totalCost >= 40 && totalCost < 100) {
-	discount = 0.07;
-	totalCost = totalCost * (1-discount);
+	discPerc = 0.07;
+	discount = totalCost * discPerc;
 } else {
-	discount = 0.12;
-	totalCost = totalCost * (1-discount);
+	discPerc = 0.12;
+	discount = totalCost * discPerc;
 };
+
+// ----- shipping calculation ----
+
+
+
+
+
+
 
 // ----- tax calculation -----
 
@@ -23,5 +58,18 @@ var tax = 0.22;
 
 	totalCost = totalCost * (1+tax);
 
-document.write(totalCost);
+
+
+
+
+// ----- FINAL CALCULATION ----
+
+var cartTotal= sumUp (cart);
+console.log(cartTotal);
+
+
+
+
+sumUp (shipping);
+
 
